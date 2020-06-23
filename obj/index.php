817ad2e636d9475dc8953afa83a9094fd5ed883f
A-Kija <42836152+A-Kija@users.noreply.github.com>
2020-06-23 12:13:08 +0300

    grazus meskenas

--- obj/index.php
+++ obj/index.php
@@ -1,8 +1,29 @@
 <?php
 
-require __DIR__.'/MiskoTevas.php';
-require __DIR__.'/Barsukas.php';
 
+// require __DIR__.'/MiskoTevas.php';
+// require __DIR__.'/Barsukas.php';
+
+
+spl_autoload_register(function ($class) {
+
+    _dc($class);
+    $prefix = '';
+    $base_dir = __DIR__ . '/';
+
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    _dc($file);
+
+    if (file_exists($file)) {
+        require $file;
+    }
+});
 
 $barsukas1 = new Barsukas('rudas', 23);
 $barsukas2 = new Barsukas;
